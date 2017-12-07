bcd46ede17526860b50020654ca285d218291100
lee <zhangloo333@gmail.com>
2017-12-07 14:34:58 -0800

    loading page and error page

---
+++ practis-ember/app/routes/application.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+import EmberRouter from '@ember/routing/router'; // 这个不能用
+export default Ember.Route.extend({
+  ajax: Ember.inject.service(),
+
+  model() {
+    let self = this;
+    // return this.get('ajax').request('//api.github.com/users');
+    return new Ember.RSVP.Promise((resolve,reject)=>{
+      setTimeout(function(){reject(
+        {message:'error happen'}
+      )}, 2000);
+    })
+  }
+});
