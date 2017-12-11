b295f6cc6eaf46d8b4650d5342119fe915b70d7e
lee <zhangloo333@gmail.com>
2017-12-11 09:46:12 -0800

    怎么通过computing alias 拿到数据

--- practis-ember/app/controllers/application.js
+++ practis-ember/app/controllers/application.js
@@ -1,11 +1,27 @@
 import Controller from '@ember/controller';
 import Ember from 'ember';
-//method1: how to import data from router.js to controller
+/**
+method1: how to import data from router.js to controller
+通过 computing的方法
+*/
+// export default Controller.extend({
+//   testData:'this is from application ',
+//   copyData: Ember.computed('model',function(){
+//     console.log(this.get('model')[0])
+//     return this.get('model')[0].login;
+//   })
+// });
+
+
+/**
+method1: how to import data from router.js to controller
+通过 controller.setProperties(mode); 不成公
+*/
+
 export default Controller.extend({
   testData:'this is from application ',
-  copyData: Ember.computed('model',function(){
-    console.log(this.get('model')[0])
-    return this.get('model')[0].login;
-  })
-
+  copyData: Ember.computed.alias('model')
 });
+const t = { s: Ember.computed.alias('model') }
+
+console.log(t.s);
