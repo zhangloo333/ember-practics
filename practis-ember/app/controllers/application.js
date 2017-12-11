1e2860c277c5bb3a5607680c424687570febcf69
LEE <zhangloo333@gmail.com>
2017-12-10 17:34:57 -0800

    action helper的运用

--- practis-ember/app/controllers/application.js
+++ practis-ember/app/controllers/application.js
@@ -3,9 +3,14 @@ import Ember from 'ember';
 //method1: how to import data from router.js to controller
 export default Controller.extend({
   testData:'this is from application ',
+  isShowingBody: true,
   copyData: Ember.computed('model',function(){
     console.log(this.get('model')[0])
     return this.get('model')[0].login;
-  })
-
+  }),
+  actions:{
+    toggleBody(){
+      this.toggleProperty('isShowingBody');
+    }
+  }
 });
