034999bc43e842c7818916a65b54ea0e702d2e4c
LEE <zhangloo333@gmail.com>
2017-12-13 20:23:48 -0800

    update new

--- practis-ember/app/controllers/application.js
+++ practis-ember/app/controllers/application.js
@@ -2,14 +2,13 @@ import Controller from '@ember/controller';
 import Ember from 'ember';
 //method1: how to import data from router.js to controller
 export default Controller.extend({
-  testData:'this is from application ',
+  testData: 'this is from application ',
   isShowingBody: true,
-  copyData: Ember.computed('model',function(){
-    console.log(this.get('model')[0])
+  copyData: Ember.computed('model', function() {
     return this.get('model')[0].login;
   }),
-  actions:{
-    toggleBody(){
+  actions: {
+    toggleBody() {
       this.toggleProperty('isShowingBody');
     }
   }
--- practis-ember/app/helpers/make-tag.js
+++ practis-ember/app/helpers/make-tag.js
@@ -1,3 +1,5 @@
+import Ember from "ember";
+
 export default Ember.Helper.helper(function(params) {
   return Ember.String.htmlSafe(`<b>${params[0]}</b>`);
 });
